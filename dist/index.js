f4248b226442382a10b78e36854bac1855266259
Teun Kelting <teun@orbiter.digital>
2021-03-16 16:39:12 +0100

    Test logging

--- dist/index.js
+++ dist/index.js
@@ -11,6 +11,7 @@ const github = __nccwpck_require__(177);
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
+  console.log('aaaa?')
   console.log(`Hellotjes ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
